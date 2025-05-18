d873cc5a425db26f7e15eb018195841be641de13
Kisvarda <kisvardazaloghaz@gmail.com>
2025-05-18 10:51:59 +0000

    First functioning sample.

--- Dnn.Kisvarda.Aukcio/Controllers/ItemController.cs
+++ Dnn.Kisvarda.Aukcio/Controllers/ItemController.cs
@@ -33,7 +33,7 @@ namespace Kisvarda.Dnn.Dnn.Kisvarda.Aukcio.Controllers
         {
                
                 
-                var item1 = ItemManager.Instance.GetItem(1, ModuleContext.ModuleId);
+                var item1 = ItemManager.Instance.GetItem(2, ModuleContext.ModuleId);
 
 
                 if (item1 == null)
@@ -44,26 +44,7 @@ namespace Kisvarda.Dnn.Dnn.Kisvarda.Aukcio.Controllers
                 return View(new List<Item> { item1 });
         }
 
-        // public ActionResult Auctions()
-        //{
-
-        //    Item newItem = new Item()
-        //    {
-        //        ItemId = 1,
-        //        ItemName = "teszt",
-        //        ItemDescription = "teszt",
-        //        ImageUrl = "facebook.com",
-        //        ModuleId = 1,
-        //        HighestBid = 100,
-        //        HighestBidUserId = 2,
-        //        AuctionEndTime = DateTime.Now,
-        //        MinimumBidIncrement = 1,
-        //        StartingPrice = 1
-        //    };
-
-        //    return View(newItem);
-        //}
-
+       
         [HttpPost]
         [System.Web.Mvc.ValidateAntiForgeryToken]
         public ActionResult Auctions(int? ItemId, int? UserId, decimal? BidAmount)
